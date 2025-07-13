cafe056ce90c1fda8da29c29197ce643925f57f1
knoph1 <knopholuoch@yahoo.com>
2025-07-13 21:43:02 +0300

    9-add.js commit

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -52,3 +52,10 @@ if (isNaN(size)) {
   }
 }
 
+// 9-add.js
+function add(a, b) {
+  return a + b;
+}
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+console.log(add(a, b));
