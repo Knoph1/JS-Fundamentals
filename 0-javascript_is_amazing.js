37dfc2ec4aa4512c1b72d943eea8dd5d6cc8ff02
knoph1 <knopholuoch@yahoo.com>
2025-07-13 21:39:09 +0300

    2-arguments.js commit

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -5,3 +5,13 @@ console.log(myVar);
 // 1-multi_languages.js
 console.log("C is fun\nPython is cool\nJavaScript is amazing");
 
+// 2-arguments.js
+const args2 = process.argv.slice(2);
+if (args2.length === 0) {
+  console.log("No argument");
+} else if (args2.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+
